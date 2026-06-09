cbe4a5ef3e5a66c42aa749135c0a8eca56d5c9fb
hwc7260-hue <hwc7260@gmail.com>
2026-06-09 13:52:37 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
@@ -13,6 +13,8 @@ import streamlit as st
 st.title("소설 생성기")
 subject = st.text_input("소설의 주제를 입력하세요")
 st.write("소설의 주제 : " + subject)
+
 if st.button("생성하기"):
-    result = chat_model.invoke( subject + "에 대한 소설을 써줘")
-    st.write(result.content)
+    with st.spinner("소설을 생성하는 중입니다..."):
+        result = chat_model.invoke( subject + "에 대한 소설을 써줘")
+        st.write(result.content)
